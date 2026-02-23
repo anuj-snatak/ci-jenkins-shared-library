fd0621f56be3963c33a8fd5adcb3c68bff14def3
anuj-snatak <anuj.jain@mygurukulam.co>
2026-02-23 23:51:46 +0530

    Update jenkins_user_provision_ci.groovy

--- src/opstree/ci/templates/jenkins_user_provision_ci.groovy
+++ src/opstree/ci/templates/jenkins_user_provision_ci.groovy
@@ -2,7 +2,6 @@ package opstree.ci.templates
 
 import opstree.common.python_env
 import opstree.common.script_loader
-import opstree.common.credential_helper
 import opstree.notification.slack_notify
 
 class jenkins_user_provision_ci implements Serializable {
@@ -17,32 +16,51 @@ class jenkins_user_provision_ci implements Serializable {
 
     void execute() {
 
+        // Checkout job repository
         steps.stage('Checkout') {
             steps.checkout steps.scm
         }
 
+        // Load Python script from shared library resources
         steps.stage('Load Script') {
             new script_loader(steps)
                 .load("python/provision_jenkins_users.py",
                       "provision_jenkins_users.py")
         }
 
+        // Setup Python virtual environment
         steps.stage('Setup Python') {
             new python_env(steps).setup()
         }
 
+        // Provision users
         steps.stage('Provision Users') {
 
-            def credHelper = new credential_helper(steps)
+            steps.withCredentials([
+                steps.string(credentialsId: config.adminCreds,
+                             variable: 'ADMIN_TOKEN'),
+                steps.usernamePassword(
+                    credentialsId: config.smtpCreds,
+                    usernameVariable: 'SMTP_USER',
+                    passwordVariable: 'SMTP_PASSWORD'
+                )
+            ]) {
 
-            credHelper.withSMTP(config.smtpCreds) {
-                steps.sh '''
-                    . venv/bin/activate
-                    python provision_jenkins_users.py
-                '''
+                steps.withEnv([
+                    "JENKINS_URL=${config.jenkinsUrl}",
+                    "ADMIN_USER=${config.adminUser}",
+                    "CSV_PATH=${config.csvPath ?: 'users.csv'}"
+                ]) {
+
+                    steps.sh '''
+                        . venv/bin/activate
+                        python provision_jenkins_users.py
+                    '''
+                }
             }
         }
 
+        // Success notification
         steps.stage('Success Notification') {
             new slack_notify(steps)
                 .send("Jenkins User Provisioning SUCCESS", "good")
