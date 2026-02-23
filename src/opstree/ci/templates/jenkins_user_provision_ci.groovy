0566df40e723770768645cfc200eb05d14eb8da1
anuj-snatak <anuj.jain@mygurukulam.co>
2026-02-23 23:33:12 +0530

    Update jenkins_user_provision_ci.groovy

--- src/opstree/ci/templates/jenkins_user_provision_ci.groovy
+++ src/opstree/ci/templates/jenkins_user_provision_ci.groovy
@@ -17,72 +17,35 @@ class jenkins_user_provision_ci implements Serializable {
 
     void execute() {
 
-        steps.pipeline {
-            agent any
+        steps.stage('Checkout') {
+            steps.checkout steps.scm
+        }
 
-            environment {
-                JENKINS_URL = config.jenkinsUrl
-                ADMIN_USER  = config.adminUser
-                ADMIN_TOKEN = steps.credentials(config.adminCreds)
-                CSV_PATH    = config.csvPath ?: "users.csv"
+        steps.stage('Load Script') {
+            new script_loader(steps)
+                .load("python/provision_jenkins_users.py",
+                      "provision_jenkins_users.py")
+        }
+
+        steps.stage('Setup Python') {
+            new python_env(steps).setup()
+        }
+
+        steps.stage('Provision Users') {
+
+            def credHelper = new credential_helper(steps)
+
+            credHelper.withSMTP(config.smtpCreds) {
+                steps.sh '''
+                    . venv/bin/activate
+                    python provision_jenkins_users.py
+                '''
             }
+        }
 
-            stages {
-
-                stage('Checkout') {
-                    steps {
-                        checkout scm
-                    }
-                }
-
-                stage('Load Script') {
-                    steps {
-                        script {
-                            new script_loader(steps)
-                                .load("python/provision_jenkins_users.py",
-                                      "provision_jenkins_users.py")
-                        }
-                    }
-                }
-
-                stage('Setup Python') {
-                    steps {
-                        script {
-                            new python_env(steps).setup()
-                        }
-                    }
-                }
-
-                stage('Provision Users') {
-                    steps {
-                        script {
-                            def credHelper = new credential_helper(steps)
-
-                            credHelper.withSMTP(config.smtpCreds) {
-                                steps.sh '''
-                                    . venv/bin/activate
-                                    python provision_jenkins_users.py
-                                '''
-                            }
-                        }
-                    }
-                }
-            }
-
-            post {
-                success {
-                    script {
-                        new slack_notify(steps)
-                            .send("Jenkins User Provisioning SUCCESS", "good")
-                    }
-                }
-                failure {
-                    script {
-                        new slack_notify(steps)
-                            .send("Jenkins User Provisioning FAILED", "danger")
-                    }
-                }
-            }
+        steps.stage('Success Notification') {
+            new slack_notify(steps)
+                .send("Jenkins User Provisioning SUCCESS", "good")
         }
     }
 }
